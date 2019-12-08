30830cba56cc6f827f61b9e3c2fd99df7ac08eb5
myron <jiang_myron@sina.com>
2019-12-08 17:11:19 +0800

    first commit file

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include<studio.h>
+
+int main()
+{int a,b,c;
+printf("input your number\n",&a);
+scanf("%d",a);
+printf("input his number\n",&b);
+scanf("%d",b);
+
+c=a+b;
+
+printf("%d\n",c);
+
+return 0;
+
+}
